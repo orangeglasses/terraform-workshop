41514f4d15943e6a4b83c1eb1aff77bad28ced08
Michael Meelis <michael.meelis@gmail.com>
2023-03-06 12:39:54 +0200

    examples

--- main.tf
+++ main.tf
@@ -14,7 +14,34 @@ provider "minikube" {
 resource "minikube_cluster" "docker" {
   driver       = "docker"
   cluster_name = "terraform-workshop"
-  addons = [
+  addons       = [
     "default-storageclass",
   ]
+}
+
+provider "kubernetes" {
+  host = minikube_cluster.docker.host
+
+  client_certificate     = minikube_cluster.docker.client_certificate
+  client_key             = minikube_cluster.docker.client_key
+  cluster_ca_certificate = minikube_cluster.docker.cluster_ca_certificate
+}
+
+provider "helm" {
+  kubernetes {
+    client_certificate     = minikube_cluster.docker.client_certificate
+    client_key             = minikube_cluster.docker.client_key
+    cluster_ca_certificate = minikube_cluster.docker.cluster_ca_certificate
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "text_file" {
+  filename = "text.txt"
+  content = "Some text"
+}
+
+module "argocd" {
+  source = "./modules/argocd"
 }
---
+++ modules/examples/main.tf
@@ -0,0 +1,22 @@
+locals {
+  template_vars = {
+    test = "value of the test var"
+    random = random_string.not_so_secret.result
+    object = {
+      inside = "string inside the object"
+    }
+  }
+}
+resource "random_password" "secret" {
+  length = 20
+}
+
+resource "random_string" "not_so_secret" {
+  length = 20
+}
+
+resource "local_file" "templated" {
+  filename = "template.txt"
+  content = templatefile("${path.module}/resources/template.txt", local.template_vars )
+}
+
