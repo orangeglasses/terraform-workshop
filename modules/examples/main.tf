fffd7d3c893c6917e5e4f151c5221d03751d8b9e
Michael Meelis <michael.meelis@gmail.com>
2023-03-06 15:29:12 +0200

    Added splat operator example

--- modules/examples/main.tf
+++ modules/examples/main.tf
@@ -19,3 +19,13 @@ resource "local_file" "templated" {
   filename = "template.txt"
   content  = templatefile("${path.module}/resources/template.txt", local.template_vars)
 }
+
+resource "random_string" "splat" {
+  count  = 5
+  length = 5
+}
+
+resource "local_file" "splat" {
+  filename = "splat.txt"
+  content  = join(", ", random_string.splat[*].result)
+}
